6de655bc7b53523e64e125eafd00fdd5e6e8abd4
Aristide Monkam <aristide.monkam@hogoplaces.com>
2026-01-28 20:55:50 +0100

    chore: add comand line runner

--- src/main/java/com/maknom/eco/guard/EcoGuardApplication.java
+++ src/main/java/com/maknom/eco/guard/EcoGuardApplication.java
@@ -1,10 +1,13 @@
 package com.maknom.eco.guard;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 @SpringBootApplication(exclude = {    org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
@@ -14,7 +17,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableCaching
 public class EcoGuardApplication {
 
+   private JdbcTemplate jdbcTemplate;
+
    public static void main(String[] args) {
       SpringApplication.run(EcoGuardApplication.class, args);
+   }
+
+   @Bean
+   CommandLineRunner initDatabase(JdbcTemplate jdbcTemplate) {
+      return args -> {
+         jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS postgis;");
+      };
    }
 }
