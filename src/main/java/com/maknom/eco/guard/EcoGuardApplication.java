246703903e61b568d7e3ddbd7babd5729883f146
Aristide Monkam <aristide.monkam@hogoplaces.com>
2026-01-28 23:47:06 +0100

    liquibase

--- src/main/java/com/maknom/eco/guard/EcoGuardApplication.java
+++ src/main/java/com/maknom/eco/guard/EcoGuardApplication.java
@@ -1,13 +1,10 @@
 package com.maknom.eco.guard;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 
 @SpringBootApplication(exclude = {    org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
@@ -16,17 +13,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @EntityScan(basePackages = "com.maknom.eco.guard.model")
 @EnableCaching
 public class EcoGuardApplication {
-
-   private JdbcTemplate jdbcTemplate;
-
    public static void main(String[] args) {
       SpringApplication.run(EcoGuardApplication.class, args);
    }
 
-   @Bean
-   CommandLineRunner initDatabase(JdbcTemplate jdbcTemplate) {
-      return args -> {
-         jdbcTemplate.execute("CREATE EXTENSION IF NOT EXISTS postgis;");
-      };
-   }
 }
